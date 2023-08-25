aa7a8708d8a9cf6065039c7fc10f56393c8d4d74
jorjGizer <125866680+jorjGizer@users.noreply.github.com>
2023-08-25 17:06:18 +0300

    remove main branch

--- spring-crud/src/main/java/org/example/config/OpenApiConfig.java
+++
@@ -1,8 +0,0 @@
-package org.example.config;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-//@Configuration
-//@ComponentScan(basePackages = {"org.springdoc", "org.example"})
-public class OpenApiConfig {
-
-}
